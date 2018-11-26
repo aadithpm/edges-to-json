baa5eaf0168c8ac615f001d5d9e5966a5504d6e4
aadithpm <aadithpm@gmail.com>
2018-11-26 02:28:16 -0600

    Dunno

--- script.py
+++ script.py
@@ -51,9 +51,6 @@ def make_json_data(in_file, out_file):
 
     nodes = []
     for entry in data_list:
-        id = int(entry[4])
-        if id not in nodes:
-            nodes.append(id)
         id = int(entry[2])
         if id not in nodes:
             nodes.append(id)
